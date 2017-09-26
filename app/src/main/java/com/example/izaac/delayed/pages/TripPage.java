d2a4f7121dfe1727ee9b3690d2549b2bbd94f0c3
izaac <izaacmarccrooke16@gmail.com>
2017-09-27 01:39:15 +1300

    Added delays to the Trippage Added Next stop name to Trippage Added Eta to TripPage Added Long Name to TripPage

--- app/src/main/java/com/example/izaac/delayed/pages/TripPage.java
+++ app/src/main/java/com/example/izaac/delayed/pages/TripPage.java
@@ -17,6 +17,9 @@ public class TripPage extends AppCompatActivity {
 
     EditText TripHeading;
     EditText Trip_long_name;
+    EditText Trip_delay;
+    EditText Trip_next_stop;
+    EditText Trip_eta;
     TripDetails tripDetails = new TripDetails();
     Trip trip = new Trip();
     private String trip_text;
@@ -33,17 +36,31 @@ public class TripPage extends AppCompatActivity {
         check();
 
         System.out.println("stop");
-        trip_text = "Trip ID:".concat(tripDetails.getTrip());
+        trip_text = "Trip ID:".concat(BaseTripDetails.get(tripNumber).getRoute_short_name());
 
-        TripHeading = (EditText) findViewById(R.id.trip1_long_name);
-        TripHeading.setText("Trip ID:", TextView.BufferType.EDITABLE);
+        TripHeading = (EditText) findViewById(R.id.trip_short_name);
+        TripHeading.setText(trip_text, TextView.BufferType.EDITABLE);
         TripHeading.setEnabled(false);
 
-        Trip_long_name = (EditText) findViewById(R.id.trip1_delay_time);
+        Trip_long_name = (EditText) findViewById(R.id.trip_long_name);
         //delay = NSDetails.get(0).getDelay();
         Trip_long_name.setText(BaseTripDetails.get(tripNumber).getRoute_long_name(), TextView.BufferType.EDITABLE);
         //Trip_long_name.setEnabled(false);
         //delay = 0;
+
+        Trip_delay = (EditText) findViewById(R.id.trip_delay);
+        delay = NSDetails.get(tripNumber).getDelay();
+        Trip_delay.setText(Integer.toString(delay), TextView.BufferType.EDITABLE);
+        Trip_delay.setEnabled(false);
+        delay = 0;
+
+        Trip_next_stop = (EditText) findViewById(R.id.trip_next_stop);
+        Trip_next_stop.setText(NSDetails.get(tripNumber).getName());
+        Trip_next_stop.setEnabled(false);
+
+        Trip_eta = (EditText) findViewById(R.id.trip_eta);
+        Trip_eta.setText(NSDetails.get(tripNumber).getEta());
+        Trip_eta.setEnabled(false);
 
     }
 
